d970ede63c790e6bfc4dd1c984af5f56c1fe3065
Zack Hobson <zack@opensourcery.com>
2009-05-15 08:50:29 -0700

    Use shoulda for tests.

--- test/model_factory_test.rb
+++ test/model_factory_test.rb
@@ -51,86 +51,86 @@ class ModelFactoryTestFactory
 end
 
 class ModelFactoryTest < Test::Unit::TestCase
-  test "protected attributes" do
+  should "initialize protected attributes" do
     m = ModelFactoryTestFactory.new_model_factory_test_protected(:baz => 4)
     assert_equal 4, m.baz
   end
   
-  test "dynamic protected attributes" do
+  should "initialize dynamic protected attributes" do
     i = 1
     m = ModelFactoryTestFactory.new_model_factory_test_protected(:bar => 3, :baz => lambda { i += 1 } )
     assert_equal 2, m.baz
   end
 
-  test "accessible attributes" do
+  should "initialize accessible attributes" do
     m = ModelFactoryTestFactory.new_model_factory_test_accessible(:baz => 4)
     assert_equal 4, m.baz
   end
   
-  test "dynamic accessible attributes" do
+  should "initialize dynamic accessible attributes" do
     i = 1
     m = ModelFactoryTestFactory.new_model_factory_test_accessible(:bar => 3, :baz => lambda { i += 1 } )
     assert_equal 2, m.baz
   end
 
-  test "new with no arguments and nested defaults" do
+  should "initialize with no arguments and nested defaults" do
     m = ModelFactoryTestFactory.new_model_factory_test_nest_def
     assert m.nest.id > 0
     assert m.id > 0
   end
 
-  test "new with arguments and nested defaults" do
+  should "intialize with arguments and nested defaults" do
     m = ModelFactoryTestFactory.new_model_factory_test_nest_def(:bar => 'foo')
     assert_equal 'foo', m.bar
     assert m.nest.id > 0
     assert m.id > 0
   end
 
-  test "new with no arguments and explicit defaults" do
+  should "intialize with no arguments and explicit defaults" do
     m = ModelFactoryTestFactory.new_model_factory_test_def
     assert_equal 'baz', m.bar
     assert m.id > 0
   end
 
-  test "new with arguments and explicit defaults" do
+  should "intialize with arguments and explicit defaults" do
     m = ModelFactoryTestFactory.new_model_factory_test_def(:bar => 'foo')
     assert_equal 'foo', m.bar
     assert m.id > 0
   end
 
-  test "create with arguments and explicit defaults" do
+  should "create with arguments and explicit defaults" do
     instance = ModelFactoryTestDef.new
     ModelFactoryTestDef.expects(:create!).with(:bar => 'foo').returns(instance)
     ModelFactoryTestFactory.create_model_factory_test_def(:bar => 'foo')
   end
 
-  test "create with no arguments and explicit defaults" do
+  should "create with no arguments and explicit defaults" do
     instance = ModelFactoryTestDef.new
     ModelFactoryTestDef.expects(:create!).with(:bar => 'baz').returns(instance)
     ModelFactoryTestFactory.create_model_factory_test_def
   end
 
-  test "new with no arguments and no defaults" do
+  should "initialize with no arguments and no defaults" do
     instance = ModelFactoryTestNoDef.new
     ModelFactoryTestNoDef.expects(:new).returns(instance)
     m = ModelFactoryTestFactory.new_model_factory_test_no_def
     assert m.id > 0
   end
 
-  test "new with arguments and no defaults" do
+  should "initialize with arguments and no defaults" do
     instance = ModelFactoryTestNoDef.new
     ModelFactoryTestNoDef.expects(:new).with(:bar => 'foo').returns(instance)
     m = ModelFactoryTestFactory.new_model_factory_test_no_def(:bar => 'foo')
     assert m.id > 0
   end
 
-  test "create with no arguments and no defaults" do
+  should "create with no arguments and no defaults" do
     instance = ModelFactoryTestNoDef.new
     ModelFactoryTestNoDef.expects(:create!).returns(instance)
     ModelFactoryTestFactory.create_model_factory_test_no_def
   end
 
-  test "create with arguments and no defaults" do
+  should "create with arguments and no defaults" do
     instance = ModelFactoryTestNoDef.new
     ModelFactoryTestNoDef.expects(:create!).with(:bar => 'foo').returns(instance)
     ModelFactoryTestFactory.create_model_factory_test_no_def(:bar => 'foo')
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,6 @@
 require 'test/unit'
 require 'rubygems'
 require 'mocha'
+require 'shoulda'
 require File.dirname(__FILE__) + '/../lib/model_factory'
 
-class << Test::Unit::TestCase
-  def test(name, &block)
-    test_name = "test_#{name.gsub(/[\s\W]/,'_')}"
-    raise ArgumentError, "#{test_name} is already defined" if self.instance_methods.include? test_name
-    define_method test_name, &block
-  end
-end
