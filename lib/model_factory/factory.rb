fbf5ba5f9900a1fc4d4c2ebbe3190da10427a5ff
Zack Hobson <zack@opensourcery.com>
2009-05-15 14:53:12 -0700

    Named factory configurations.
     ModelFactory.configure do
       favorite(Widget) {|w| w.name = 'my favorite' }
     end
    
     Widget.factory.create_favorite.name # => 'my favorite'

--- lib/model_factory/factory.rb
+++ lib/model_factory/factory.rb
@@ -7,18 +7,40 @@ module ModelFactory
     end
 
     def create(opt = {}, &block)
-      instance = new(opt, &block)
+      create_named(:default, opt, &block)
+    end
+
+    def new(opt = {}, &block)
+      new_named(:default, opt, &block)
+    end
+
+    def method_missing(method, opt = {}, &block)
+      case method.to_s
+      when /^create_(.+)$/
+        create_named($1.to_sym, opt, &block)
+      when /^new_(.+)$/
+        new_named($1.to_sym, opt, &block)
+      else
+        raise NameError, "no such method `#{method}'"
+      end
+    end
+
+   private
+
+    def new_named(name, opt = {})
+      instance = @class.new
+      @options[name].call(instance) if @options[name]
+      opt.each {|k,v| instance.send("#{k}=", v) }
+      yield instance if block_given?
+      instance
+    end
+
+    def create_named(name, opt = {}, &block)
+      instance = new_named(name, opt, &block)
       instance.save!
       instance
     end
 
-    def new(opt = {})
-      instance = @class.new
-      @options[:default].call(instance) if @options[:default]
-      opt.each {|k,v| instance.send("#{k}=", v) }
-      instance
-    end
   end
-
 
 end
--- test/model_factory_test.rb
+++ test/model_factory_test.rb
@@ -1,6 +1,5 @@
 require File.dirname(__FILE__) + '/test_helper'
 
-
 class ModelFactoryTest < Test::Unit::TestCase
 
   should "not instantiate when using new" do
@@ -32,6 +31,35 @@ class ModelFactoryTest < Test::Unit::TestCase
   
     should "initialize with an unspecified argument" do
       assert_equal 4, Widget.factory.create(:price => 4.0).price
+    end
+  end
+
+  context "with a named factory" do
+    setup do
+      ModelFactory.configure do
+        default(Widget) {|w| w.name = 'wubbo' }
+        favorite(Widget) {|w| w.name = 'foobaz' }
+      end
+    end
+  
+    should "not apply named config to default" do
+      assert_equal 'wubbo', Widget.factory.create.name
+    end
+  
+    should "not apply any config to unrecognized name" do
+      assert_equal nil, Widget.factory.create_emptyfoo.name
+    end
+  
+    should "initialize defaults with no arguments" do
+      assert_equal 'foobaz', Widget.factory.create_favorite.name
+    end
+  
+    should "initialize defaults with an unspecified argument" do
+      assert_equal 'foobaz', Widget.factory.create_favorite(:price => 4.0).name
+    end
+  
+    should "initialize with an unspecified argument" do
+      assert_equal 4, Widget.factory.create_favorite(:price => 4.0).price
     end
   end
 end
