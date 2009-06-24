589ee1bf376ef05a4974bc9ab4c5d9f809098a2f
Zack Hobson <zack@opensourcery.com>
2009-06-24 16:48:26 -0700

    Fixed non-ORM test case for 1.9.1.

--- test/non_orm_test.rb
+++ test/non_orm_test.rb
@@ -2,21 +2,21 @@ require File.dirname(__FILE__) + '/test_helper'
 require File.dirname(__FILE__) + '/../lib/modelfactory'
 
 class ModelFactoryTest < Test::Unit::TestCase
-  class Foo < Struct.new('Foo', :name); end
+  foo = Struct.new('Foo', :name)
 
   context "configured for a non-relational type" do
     setup do
       ModelFactory.configure do
-        default(Foo) { name { 'wubbo' } }
+        default(foo) { name { 'wubbo' } }
       end
     end
   
     should "apply config to a created instance" do
-      assert_equal 'wubbo', ModelFactory[Foo].create.name
+      assert_equal 'wubbo', ModelFactory[foo].create.name
     end
 
     should "apply config to a new instance" do
-      assert_equal 'wubbo', ModelFactory[Foo].new.name
+      assert_equal 'wubbo', ModelFactory[foo].new.name
     end
   end
 end
